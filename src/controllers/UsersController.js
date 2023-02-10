dfd1fd987a93e6059ec77ac5aff610ffbe01183f
Adryann Layon <adryann.layon7@gmail.com>
2023-02-10 13:45:13 -0300

    uso do SELECT no JS

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,22 @@
 const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
+
 class UsersController {
-  create(request, response) {
+  async create(request, reponse) {
     const { name, email, password } = request.body
 
-    if (!name) {
-      throw new AppError('Nome é obrigatório!')
+    const database = await sqliteConnection()
+
+    const checkUserExist = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (checkUserExist) {
+      throw new AppError('Este e-mail já está em uso.')
     }
 
-    response.status(201).json({ name, email, password })
+    return reponse.status(201).json()
   }
 }
 
