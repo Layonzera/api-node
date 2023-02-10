895857c7e8b4d676ed4f6aafd82bc723982b3ab6
Adryann Layon <adryann.layon7@gmail.com>
2023-02-10 13:00:57 -0300

    migrations: automatizando a criação de tabelas

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 require('express-async-errors')
-const database = require('./database/sqlite')
+const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
 const express = require('express')
 const routes = require('./routes')
 const app = express()
 
+migrationsRun()
+
 app.use(express.json())
 app.use(routes)
-
-database()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
